645160ac58d336543a775c4c6f017477fb03fe8d
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-03-25 15:25:57 +0100

    ajout try catch

--- Candidature/Server/Router/routerback.js
+++ Candidature/Server/Router/routerback.js
@@ -17,10 +17,10 @@ router.post("/authentification/register", async (req, res) => {
 
 // Connexion
 router.post('/authentification/login', async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user || user.password !== password) {
-      return res.status(401).json({ error: 'Echec identification' });
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user || user.password !== password) {
+            return res.status(401).json({ error: 'Echec identification' });
     }
     req.session.userId = user._id;
     res.json({ message: 'Connexion reussie' });
@@ -28,20 +28,29 @@ router.post('/authentification/login', async (req, res) => {
 
 // Deconnexion
 router.post('authentification/logout', (req, res) => {
+try {
     req.session.destroy();
     res.json({messsage: 'Deconnexion reussie'});
+} catch (error) {
+    res.status(500).json({error: 'Echec deconnexion'});
+}
 });
 
 // Récupération des candidatures
 router.get('/candidature', async (req, res) => {
+try{
     if (!req.session.userId)
         return res.status(401).json({error: 'Erreur authentification'});
     const application = await application.find({userId: req.session.userId});
     res.json(application);
+} catch (error) {
+    res.status(500).json({error: 'Erreur lors de la récuperation des candidatures'});
+}
 });
 
 // Récupération d'une candidature spécifique
 router.get('/candidature/:id', async (req, res) => {
+try {
     if (!req.session.userId)
         return res.status(401).json({error: 'Erreur authentification'});
     const application = await application.findById(req.params.id);
@@ -49,30 +58,45 @@ router.get('/candidature/:id', async (req, res) => {
         return res.status(404).json({error: 'Candidature non trouvée'});
     }
     res.json(application);
+} catch (error) {
+    res.status(500).json({error: 'Erreur lors de la récuparation de la candidature'});
+}
 });
 
 // Ajout candidature
 router.post('/candidature', async (req, res) => {
+try{
     if (!req.session.userId)
         return res.status(401).json({error: 'Erreur authentification'});
     const application = await application.create({...req.body, userId: req.session.userId});
     res.status(401).json(application);
+} catch (error) {
+    res.status(500).json({error: 'Erreur ajout candidature'});
+}
 });
 
 // Mise à jour d' une candidature
 router.put('application/:id', async (req, res) => {
+try {
     if (!req.session.userId)
         return res.status(401).json({error: 'Erreur authentification'});
     const application = await application.findByIdAndUpdate(req.params.id, req.body, {new : true});
     res.json(application);
+} catch (error) {
+    res.status(500).json({error: 'Echec lors de la mise à jour de la candidature'});
+}
 });
 
 // Suppression d'une candidature
 router.delete('/application/:id', async (req, res) => {
-    if (!req.session.userId)
-        return res.status(401).json({error: 'Erreur authentification'});
-await application.findByIdAndDelete(req.params.id);
-res.json({message: 'Candidature supprimée'});
+    try {
+        if (!req.session.userId)
+            return res.status(401).json({error: 'Erreur authentification'});
+        await application.findByIdAndDelete(req.params.id);
+       es.json({message: 'Candidature supprimée'});
+    } catch(error) {
+        res.status(500).json({error: 'Erreur lors de la suppression'});
+    }
 });
 
 
