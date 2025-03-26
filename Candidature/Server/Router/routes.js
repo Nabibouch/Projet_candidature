36ba9243ef525b438f055d164d54d2808e3e153e
AnjaraAndriatseheno <nanjaraandria@gmail.com>
2025-03-26 14:07:57 +0100

    route count

--- Candidature/Server/Router/routes.js
+++ Candidature/Server/Router/routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { inscription, connexion, deconnexion, recupCandidatureSpé, recupCandidatures, addCandidature, majCandidature, deleteCandidature } from './controllerCandidature.js';
+import { recupCandidatureSpé, recupCandidatures, addCandidature, majCandidature, deleteCandidature, countCandidature } from './controllerCandidature.js';
+import {inscription, connexion, deconnexion} from '/.controllerLogin.js';
 
-const router = express.Router();
+ const router = express.Router();
 
 router.post('/register', inscription);
 router.post('/connexion', connexion);
@@ -11,5 +12,6 @@ router.get('/recupCandidatureSpé', recupCandidatureSpé);
 router.post('/addCandidature', addCandidature);
 router.put('/majCandidature/:id', majCandidature);
 router.delete('/deleteCandidature/:id', deleteCandidature);
+router.get('/countCandidature', countCandidature)
 
 export default router;
